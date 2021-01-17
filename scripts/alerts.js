8d6d6dd511742739a62f025f257b930d34232edf
ShadowOfNick <nikitaz13.87@gmail.com>
2021-01-17 14:28:47 +0100

    try1

---
+++ scripts/alerts.js
@@ -0,0 +1,18 @@
+function Bad() {
+  $('#bad').fadeIn(1000);
+  setTimeout(function() { 
+      $('#bad').fadeOut(1000); 
+  }, 5000);
+}
+function Good() {
+  $('#good').fadeIn(1000);
+  setTimeout(function() { 
+      $('#good').fadeOut(1000); 
+  }, 5000);
+}
+function Info() {
+  $('#info').fadeIn(1000);
+  setTimeout(function() { 
+      $('#info').fadeOut(1000); 
+  }, 5000);
+}
